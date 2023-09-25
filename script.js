377091fa14bdc9ccab043a38a55ff03ff7567331
Keri96 <kerikanyi@gmail.com>
2023-09-25 18:50:04 +0200

    login working

--- script.js
+++ script.js
@@ -19,6 +19,11 @@ btnLogin.addEventListener("click", function (e) {
   e.preventDefault();
   console.log("login");
 
+  currentAccount = account.find(
+    (acc) => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
   if (
     inputLoginUsername.value === account.name &&
     inputLoginPin.value === account.password
